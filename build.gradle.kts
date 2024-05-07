2d82c865a384e0ea3c52bb78dd71a3b1425de0fb
fssemanda <macho.francis2@gmail.com>
2024-05-07 19:23:41 +0300

    Commit to NewDev

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    val room_version = "2.6.1"
+    id("androidx.room") version "$room_version" apply false
+    id("com.google.dagger.hilt.android") version "2.47" apply false
 }
