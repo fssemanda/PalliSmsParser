196c65808d03ebd4ec4e614b9eb9a1c1ea95315a
fssemanda <macho.francis2@gmail.com>
2024-05-07 16:11:48 +0300

    First commit to devNew

--- app/src/main/java/com/mypalli/pallismsparser/dataclasses/NetworkChangeReceiver.kt
+++ app/src/main/java/com/mypalli/pallismsparser/dataclasses/NetworkChangeReceiver.kt
@@ -1,4 +1,27 @@
 package com.mypalli.pallismsparser.dataclasses
 
-class NetworkChangeReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.mypalli.pallismsparser.MainActivity
+import com.mypalli.pallismsparser.SMSViewModels.SMSViewModel
+
+class NetworkChangeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (isOnline(context)) {
+            val viewModel = ViewModelProvider(context as MainActivity)[SMSViewModel::class.java]
+            viewModel.synchronizeData()
+        }
+    }
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        Log.d("Current Network Status","$networkInfo")
+        return networkInfo != null && networkInfo.isConnected
+    }
 }
+
